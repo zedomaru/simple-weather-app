82dff18a0fbb49fb435d9489d6bea4984cdcc432
Zaki Elyasa Djauhari <zakielyasadjauhari@gmail.com>
2019-07-04 09:42:19 +0700

    add images

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,8 @@
 const cityForm = document.querySelector("form");
-const card = document.querySelector("card");
-const detail = document.querySelector(".details");
+const card = document.querySelector(".card");
+const details = document.querySelector(".details");
+const time = document.querySelector("img.time");
+const icon = document.querySelector(".icon img");
 
 const updateCity = async city => {
   const cityDetail = await getCity(city);
@@ -20,10 +22,28 @@ cityForm.addEventListener("submit", e => {
   updateCity(city)
     .then(data => {
       updateUI(data);
+      console.log(data);
     })
     .catch(err => {
       console.log(err);
     });
 });
 
-const updateUI = data => {};
+const updateUI = data => {
+  const { cityDetail, weatherDetail } = data;
+
+  //update detail template
+  details.innerHTML = `
+    <h5 class="my-3">${cityDetail.EnglishName}</h5>
+    <div class="my-3">${weatherDetail[0].WeatherText}</div>
+    <div class="display-4 my-4">
+        <span>${weatherDetail[0].Temperature.Metric.Value}</span>
+        <span>&deg;C</span>
+        </div>
+    `;
+
+  //remove d-none
+  if (card.classList.contains("d-none")) {
+    card.classList.remove("d-none");
+  }
+};
